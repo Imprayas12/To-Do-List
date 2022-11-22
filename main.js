d0954eaa93f946f6535de67702e1c14263686460
prayas gautam <prayas12gautam@gmail.com>
2022-11-22 23:39:23 +0530

    Filter added

--- main.js
+++ main.js
@@ -46,7 +46,6 @@ function loadItemList() {
 document.onload = loadItemList()
 
 document.querySelectorAll('.delete-btn').forEach((btn) => {
-    // console.log("click");
     btn.addEventListener('click',() => {
         let itemList = JSON.parse(localStorage.getItem('itemList'));
         for(let i = 0; i < itemList.length; i++) {
@@ -58,4 +57,24 @@ document.querySelectorAll('.delete-btn').forEach((btn) => {
         localStorage.setItem('itemList',JSON.stringify(itemList));
         location.reload();
     })
+})
+
+var filter = document.getElementById('filter')
+filter.addEventListener('keyup',() => {
+    if(JSON.parse(localStorage.getItem('itemList')) == 0) {
+        alert("NO TASKS TO SEARCH");
+    }
+    else {
+        input = document.getElementById('filter');
+        filter = input.value.toUpperCase();
+        var li = document.getElementsByTagName('li');
+        for (i = 0; i < li.length; i++) {
+            txtValue = li[i].textContent || li[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              li[i].style.display = "";
+            } else {
+              li[i].style.display = "none";
+            }
+          }
+    }
 })
